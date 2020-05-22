96f9797c54361b6c984523fed74b7ec67a6687c0
mmlait <miia.laitinen@hotmail.com>
2020-05-22 12:07:31 +0300

    add ContactForm component and delete contact from ui

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuid } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -11,6 +11,7 @@ import {
   FILTER_CONTACTS,
   CLEAR_FILTER
 } from '../types';
+import contactContext from './contactContext';
 
 const ContactState = props => {
   const initialState = {
@@ -41,8 +42,22 @@ const ContactState = props => {
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
+  const addContact = contact => {
+    contact.id = uuid();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  }
+
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  }
+
   return (
-    <ContactContext.Provider value={{ contacts: state.contacts }}>
+    <ContactContext.Provider 
+      value={{
+        contacts: state.contacts,
+        addContact,
+        deleteContact
+      }}>
       { props.children }
     </ContactContext.Provider>
   );
