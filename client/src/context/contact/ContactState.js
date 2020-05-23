4d82650f65989accecf917429cc6cc0efe3dbd5f
mmlait <miia.laitinen@hotmail.com>
2020-05-23 11:22:40 +0300

    update contact, add contact filter and add some animation

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,7 +37,9 @@ const ContactState = props => {
         phone: '333-333-3333',
         type: "professional"
       }
-    ]
+    ],
+    current: null,
+    filtered: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -45,18 +47,45 @@ const ContactState = props => {
   const addContact = contact => {
     contact.id = uuid();
     dispatch({ type: ADD_CONTACT, payload: contact });
-  }
+  };
 
   const deleteContact = id => {
     dispatch({ type: DELETE_CONTACT, payload: id });
-  }
+  };
+
+  const setCurrent = contact => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
+
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
+
+  const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
+
+  const filterContacts = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
+
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
 
   return (
     <ContactContext.Provider 
       value={{
         contacts: state.contacts,
+        current: state.current,
+        filtered: state.filtered,
         addContact,
-        deleteContact
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+        filterContacts,
+        clearFilter
       }}>
       { props.children }
     </ContactContext.Provider>
