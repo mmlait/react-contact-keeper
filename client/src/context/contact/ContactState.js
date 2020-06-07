3d3f10393d320b203c841afa11e1869dfb37c082
mmlait <miia.laitinen@hotmail.com>
2020-06-07 13:15:34 +0300

    Delete contact from db, update contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -63,8 +63,42 @@ const ContactState = props => {
     }
   };
 
-  const deleteContact = id => {
-    dispatch({ type: DELETE_CONTACT, payload: id });
+  const deleteContact = async id => {
+    try {
+      await axios.delete(`/api/contacts/${id}`);
+
+      dispatch({
+        type: DELETE_CONTACT,
+        payload: id
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload:  err.response.msg
+      });
+    }
+  };
+
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.put(`/api/contacts/${contact._id}`, contact, config);
+
+      dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload:  err.response.msg
+      });
+    }
   };
 
   const clearContacts = () => {
@@ -77,10 +111,6 @@ const ContactState = props => {
 
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   const filterContacts = text => {
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -22,20 +22,20 @@ export default (state, action) => {
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [...state.contacts, action.payload],
+        contacts: [action.payload, ...state.contacts],
         loading: false
       };
     case UPDATE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.map(contact => 
-          contact.id === action.payload.id ? action.payload : contact),
+          contact._id === action.payload._id ? action.payload : contact),
         loading: false
       };
     case DELETE_CONTACT:
       return {
         ...state,
-        contacts: state.contacts.filter(contact => contact.id !== action.payload),
+        contacts: state.contacts.filter(contact => contact._id !== action.payload),
         loading: false
       };
     case CLEAR_CONTACTS:
@@ -60,7 +60,6 @@ export default (state, action) => {
       return {
         ...state,
         filtered: state.contacts.filter(contact => {
-          // "gi" means global and insensitive (we want it to match whether it's uppercase or lowercase)
           const regex = new RegExp(`${action.payload}`, "gi");
           return contact.name.match(regex) || contact.email.match(regex);
         })
